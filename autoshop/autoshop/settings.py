55eabdc9ce83fa6275593abcc53bfafd244fa87a
Oleg <example@gmail.com>
2024-06-02 20:30:48 +0300

    02/06

--- autoshop/autoshop/settings.py
+++ autoshop/autoshop/settings.py
@@ -90,6 +90,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'autoparts',
+#         'USER': 'postgres',
+#         'PASSWORD': 'E_333dddd',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
